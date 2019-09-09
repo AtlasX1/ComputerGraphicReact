902ca3f9e4192c33b404b65ab49331d4f28ad194
Roman Moravskyi <moravsky12@gmail.com>
2019-09-09 16:15:18 +0300

    initial import

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Alert, Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <Alert variant="darsadfk ">
+      <Button> Login </Button>
+    </Alert>
+  );
+}
+
+export default App;
