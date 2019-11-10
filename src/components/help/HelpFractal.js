6ee09f62cc425e759f9d9ff551cbc88fc7e71749
AtlasX1 <pavlo.sergij@ukr.net>
2019-11-11 00:59:08 +0200

    Help

--- src/components/help/HelpFractal.js
+++ src/components/help/HelpFractal.js
@@ -24,7 +24,39 @@ const ModalDialog = props => {
       </Modal.Header>
       <Modal.Body>
         <h4>Instruction</h4>
-        <p>Some text or img</p>
+        <p>
+          Restore defaults - sets zoom center, zoom size and Julia constant
+          value to default values. Zoom center = [0;0], zoom size = 1.0 (no
+          zoom), Julia constant value = 0 + 0i.
+        </p>
+        <p>
+          Mandelbrot set / Julia set dropdown changes rendering fractal type to
+          corresponding.
+        </p>
+        <p>
+          Max iterations - value that represents number of calculations
+          performed for each x, y point in the plot area.
+        </p>
+        <p>Auto zoom iterations - speeds up rendering when zooming.</p>
+        <p>
+          Color scheme dropdown - changes color of rendered fractal. Two scheme
+          available: 'Cold ice' and 'Pink'.
+        </p>
+        <p>
+          When rendering Julia set, you can enter constant value for this
+          fractal in the following format: x + yi. This will change fractal
+          form.{" "}
+        </p>
+        <p>
+          '+' and '-' buttons turn on zooming. To stop zooming process press
+          left mouse button anywhere on the fractal canvas.
+        </p>
+        <p>
+          You can also zoom by clicking left mouse button on the fractal canvas.
+          Zoom size changes according to the time left mouse button has been
+          held down.
+        </p>
+        <p>To save and download press right mouse button on fractal canvas.</p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide} variant="secondary">
