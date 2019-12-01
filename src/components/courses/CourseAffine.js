fc80ac288eba2e9fd9f93c85a3d1e250660acc06
Volter3228 <47464009+Volter3228@users.noreply.github.com>
2019-12-01 15:32:23 +0200

    Add files via upload

--- src/components/courses/CourseAffine.js
+++ src/components/courses/CourseAffine.js
@@ -17,45 +17,97 @@ export default class MenuBar extends React.Component {
     super(props);
 
     this.state = {
-      valA: 0,
-      valB: 0,
-      corner: 0,
-      kof: 0
+      valAX: 0,
+      valAY: 0,
+      valBX: 100,
+      valBY: 100,
+      angle: 0,
+      kof: 1
     };
   }
-
+  clearCanvas = () => {
+    let canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d');
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+  };
+  drawRect = () => {
+    let canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d');
+    //  ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.fillRect(this.state.valAX, this.state.valAY, (this.state.valBX - this.state.valAX), (this.state.valBY - this.state.valAY));
+  };
+  rotateRect = () => {
+    let canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d'); 
+    ctx.translate(this.state.valBX - (this.state.valBX - this.state.valAX)/2,this.state.valBY - (this.state.valBY - this.state.valAY)/2);
+    ctx.rotate(this.state.angle * Math.PI/180);
+    ctx.fillRect(-((this.state.valBX - this.state.valAX)/2), -((this.state.valBY - this.state.valAY)/2), (this.state.valBX - this.state.valAX), (this.state.valBY - this.state.valAY));
+    ctx.restore();
+  };
+  scaleRect = () => {
+    let canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d'); 
+    ctx.translate(this.state.valAX,this.state.valAY);
+    ctx.scale(this.state.kof, this.state.kof);
+    ctx.restore();
+  };
+  buildRect = () => {
+    this.clearCanvas();
+    this.scaleRect();
+    this.rotateRect();
+  };
   onClearClick = () => {
     alert("Clear");
+    this.clearCanvas();
   };
   onBuildClick = () => {
     alert("Build");
+    this.clearCanvas();
+    this.drawRect();
   };
   onRotateClick = () => {
     alert("Rotate");
+    this.clearCanvas();
+    this.buildRect();
   };
   onButtonClick = () => {
     alert("Button");
+    this.clearCanvas();
+    this.buildRect();
   };
   UpClick = () => {
-    alert("Up");
+    //alert("Up");
+    this.state.valAY -= 10;
+    this.state.valBY -= 10;
+    this.buildRect();
   };
   DownClick = () => {
-    alert("Down");
+    //alert("Down");
+    this.state.valAY += 10;
+    this.state.valBY += 10;
+    this.buildRect();
   };
   LeftClick = () => {
-    alert("Left");
+    //alert("Left");
+    this.state.valAX -= 10;
+    this.state.valBX -= 10;
+    this.buildRect();
   };
   RigthClick = () => {
-    alert("Right");
+    //alert("Right");
+    this.state.valAX += 10;
+    this.state.valBX += 10;
+    this.buildRect();
   };
   onChangeAB = (AorB, value) => {
     this.setState({ [AorB]: value }, () => {
-      console.log("A: " + this.state.valA + "\nB: " + this.state.valB);
+      console.log("AX: " + this.state.valAX + "\nAY: " + this.state.valAY + "\nBX: " + this.state.valBX + "\nBY: " + this.state.valBY);
     });
   };
-  onChangeCorner = value => {
-    this.setState({ corner: value }, () => {
-      console.log("Corner: " + this.state.corner);
+  onChangeAngle = value => {
+    this.setState({ angle: value }, () => {
+      console.log("Angle: " + this.state.angle);
     });
   };
   onChangeKof = value => {
@@ -89,22 +141,48 @@ export default class MenuBar extends React.Component {
                     Square diagonal points:
                     <Row>
                       <Col>
-                        A:{" "}
+                        AX:{" "}
                         <input
                           type="number"
+                          defaultValue="0"
                           style={{ width: "80%" }}
                           onChange={v => {
-                            this.onChangeAB("valA", v.target.value);
+                            this.onChangeAB("valAX", v.target.value);
                           }}
                         />
                       </Col>
                       <Col>
-                        B:{" "}
+                        AY:{" "}
                         <input
                           type="number"
+                          defaultValue="0"
                           style={{ width: "80%" }}
                           onChange={v => {
-                            this.onChangeAB("valB", v.target.value);
+                            this.onChangeAB("valAY", v.target.value);
+                          }}
+                        />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        BX:{" "}
+                        <input
+                          type="number"
+                          defaultValue="100"
+                          style={{ width: "80%" }}
+                          onChange={v => {
+                            this.onChangeAB("valBX", v.target.value);
+                          }}
+                        />
+                      </Col>
+                      <Col>
+                        BY:{" "}
+                        <input
+                          type="number"
+                          defaultValue="100"
+                          style={{ width: "80%" }}
+                          onChange={v => {
+                            this.onChangeAB("valBY", v.target.value);
                           }}
                         />
                       </Col>
@@ -125,10 +203,11 @@ export default class MenuBar extends React.Component {
                   <ListGroup.Item>
                     <input
                       type="number"
+                      defaultValue = "0"
                       style={{ width: "100%" }}
                       placeholder="тут кут повороту"
                       onChange={v => {
-                        this.onChangeCorner(v.target.value);
+                        this.onChangeAngle(v.target.value);
                       }}
                     />
                   </ListGroup.Item>
@@ -147,6 +226,7 @@ export default class MenuBar extends React.Component {
                   <ListGroup.Item>
                     <input
                       type="number"
+                      defaultValue = "1"
                       style={{ width: "100%" }}
                       placeholder="тут кф скейлу"
                       onChange={v => {
@@ -228,7 +308,7 @@ export default class MenuBar extends React.Component {
                     Affine transformations
                   </Card.Header>
 
-                  <canvas ref="canvas" width={640} height={480} />
+                  <canvas ref="canvas" width={640} height={640} />
                 </Card>
               </CardGroup>
             </Container>
